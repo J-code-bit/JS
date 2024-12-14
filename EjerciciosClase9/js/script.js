794c435df00c842097b54c86720e6a12e47f4847
Jimena Soledad Ruiz <jimenasoledadruiz@gmail.com>
2024-12-14 12:03:24 -0300

    algunos cambios para probar Git

---
+++ EjerciciosClase9/js/script.js
@@ -0,0 +1,20 @@
+
+//Ej.1 clase 9
+
+let num1 = parseInt(prompt ("ingrese un número:")); 
+let num2 = parseInt(prompt ("ingrese otro un número:"));
+
+suma = num1 + num2;
+resta = num1 - num2;
+multiplicacion = num1 * num2;
+
+console.log('Suma:',suma);
+console.log('Resta:',resta);
+console.log('Multiplicación:',multiplicacion);
+
+alert ('Hola Mundo!');
+
+//para poner fecha y hora
+let hoy = new Date();
+console.log (hoy)
+
